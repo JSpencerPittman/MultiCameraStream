c6ae181151d372a157d2006cd9a74108dd5041ae
JSpencerPittman <jspencerpittman@gmail.com>
2024-03-07 05:13:55 -0600

    Removed nonessential code

--- src/camera.py
+++ src/camera.py
@@ -19,12 +19,3 @@ class Camera(object):
         resize = cam_json["Resize"] if "Resize" in cam_json.keys() else None
 
         return Camera(name, host, port, uri, resize)
-
-    def __str__(self):
-        uri_mention = f" from {self.uri}" if self.uri is not None else ""
-        res_mention = f" resized to {
-            self.resize}" if self.resize is not None else ""
-        return f"{self.name} on rtp:{self.host}:{self.port}{uri_mention}{res_mention}"
-
-    def __repr__(self):
-        return self.__str__()
